098bc358c22205531b987ddb5e2112903ea1d76f
Estotive <martinyuk.d.i.20@gmail.com>
2021-12-16 00:50:26 +0300

    7.1

--- src/components/Ads/NewAd.vue
+++ src/components/Ads/NewAd.vue
@@ -1,17 +1,35 @@
-<template> 
-	<v-container>
+        <template> 
+    <v-container>
 		<v-layout row> 
-<v-flex xs12>
-				<h1>Home</h1> 
-			</v-flex>
-		</v-layout> 
-	</v-container>
+            <v-flex xs12 sm6 offset-sm3>
+                <h1 class="text--secondary mb-3 mt-3">Create Ad</h1>
+                    <v-form v-model="valid" ref="form" validation>
+                        <v-text-field
+                        name="title" 
+                        label="Ad Title" 
+                        type="text" 
+                        v-model="title"
+                        :rules="[ v => !!v || 'Title is required']" 
+                        >
+                        </v-text-field>
+                    <v-textarea  
+                        name="description" 
+                        label="Ad Description" 
+                        type="text" 
+                        v-model="description"
+                        :rules="[ v => !!v || 'Description is required']"
+                        class="mb-3" 
+                        ></v-textarea>
+                    </v-form>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
 <script>
 export default {
-	data () { 
-		return {
-		} 	
-	}
-} 
+    data() {
+        return {
+        }
+    }
+}
 </script>
