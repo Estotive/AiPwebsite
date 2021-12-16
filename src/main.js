184be03ecce0bb3a223d08a392cfe58a4b33fe91
Estotive <martinyuk.d.i.20@gmail.com>
2021-12-16 03:09:30 +0300

    13.1

--- src/main.js
+++ src/main.js
@@ -4,32 +4,30 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router/index'
 import store from './store'
-// import fb from 'firebase'
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import fb from 'firebase'
 
 Vue.use(Router)
 Vue.config.productionTip = false
 
 new Vue({
-    vuetify,
-    render:h=>h(App),
-    router:router,
-    store,
-    created(){
-        var firebaseConfig = {
-  apiKey: "AIzaSyDz6k9bqs3k9-yhvy_9ElNsnj2Zj5vPyi8",
-  authDomain: "nazvanieproekta-bebde.firebaseapp.com",
-  projectId: "nazvanieproekta-bebde",
-  storageBucket: "nazvanieproekta-bebde.appspot.com",
-  messagingSenderId: "792037850649",
-  appId: "1:792037850649:web:1fd65f3b4b740ef784f488",
-  measurementId: "G-7F74VZ6ZVT"
+  vuetify,
+  render: h => h(App),
+  router:router,
+  store,
+  created(){
+    const firebaseConfig = {
+      apiKey: "AIzaSyDz6k9bqs3k9-yhvy_9ElNsnj2Zj5vPyi8",
+      authDomain: "nazvanieproekta-bebde.firebaseapp.com",
+      projectId: "nazvanieproekta-bebde",
+      storageBucket: "nazvanieproekta-bebde.appspot.com",
+      messagingSenderId: "792037850649",
+      appId: "1:792037850649:web:1fd65f3b4b740ef784f488",
+      measurementId: "G-7F74VZ6ZVT"
     };
-    // Initialize Firebase
-        // fb.initializeApp(firebaseConfig);
-        // fb.analytics();
-        const app = initializeApp(firebaseConfig);
-        getAnalytics(app);
-    }  
+    
+  // Initialize Firebase
+  fb.initializeApp(firebaseConfig);
+  fb.analytics();
+
+}
 }).$mount('#app')
